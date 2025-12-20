21c3e74af3f44d4808421b778dfe106fe706fa58
mujahidraj <mujahidraj65@gmail.com>
2025-12-20 19:38:16 +0600

    again

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -7,16 +7,16 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey:"AIzaSyBh4RxAYCLCtsAlN-oIaG-y4aTdYTFZD9I",
-  authDomain:"juta-express-v2.firebaseapp.com",
-  projectId:"juta-express-v2",
-  storageBucket:"juta-express-v2.firebasestorage.app",
-  messagingSenderId:"579145883166",
-  appId:"1:579145883166:web:18584048aa6ed64900580f",
-  measurementId:"G-8R6GND7VNQ"
+  apiKey: "AIzaSyAXxPJ1KAXSPAojNp2QBziQISv1oI8WHWY",
+  authDomain: "juta-express.firebaseapp.com",
+  projectId: "juta-express",
+  storageBucket: "juta-express.firebasestorage.app",
+  messagingSenderId: "224517648228",
+  appId: "1:224517648228:web:88de28bcf4ea6010325471",
+  measurementId: "G-CHHRCM87QD"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 // eslint-disable-next-line no-unused-vars
 const analytics = getAnalytics(app);
