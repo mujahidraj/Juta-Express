6f4038e75f3ee3a934430466b8ed6c8a85422a22
mujahidraj <mujahidraj65@gmail.com>
2025-12-20 19:25:07 +0600

    aga

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: "juta-express-v2",
-  storageBucket:import.meta.env.VITE_storageBucket,
-  messagingSenderId:import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId
+    apiKey:"AIzaSyBh4RxAYCLCtsAlN-oIaG-y4aTdYTFZD9I",
+  authDomain:"juta-express-v2.firebaseapp.com",
+  projectId:"juta-express-v2",
+  storageBucket:"juta-express-v2.firebasestorage.app",
+  messagingSenderId:"579145883166",
+  appId:"1:579145883166:web:18584048aa6ed64900580f",
+  measurementId:"G-8R6GND7VNQ"
 };
 
 // Initialize Firebase
